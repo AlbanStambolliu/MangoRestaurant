751b7fbcba57740b938b2e31bf240d4a17827eb6
AlbanStambolliu <AlbanStambolliu@gmail.com>
2023-09-08 21:21:44 +0200

    Commiting all files

---
+++ Mango.Services.ProductAPI/MappingConfig.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.Dto;
+
+namespace Mango.Services.ProductAPI
+{
+	public class MappingConfig
+	{
+		public static MapperConfiguration RegisterMaps()
+		{
+			var mappingConfig = new MapperConfiguration(config =>
+			{
+				config.CreateMap<ProductDto, Product>();
+				config.CreateMap<Product, ProductDto>();
+				
+				//Alternative we can use ReverseMap instead of two lines of code
+				//config.CreateMap<Product, ProductDto>().ReverseMap();
+			});
+			return mappingConfig;
+		}
+	}
+}
---
+++ Mango.Web/Controllers/ProductController.cs
@@ -0,0 +1,106 @@
+﻿using Mango.Web.Models;
+using Mango.Web.Services;
+using Mango.Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Mango.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productServices;
+
+        public ProductController(IProductService productServices)
+        {
+            this._productServices = productServices;
+        }
+        public async Task<IActionResult> ProductIndex()
+        {
+            List<ProductDto> products = new List<ProductDto>();
+            var response = await _productServices.GetAllProductAsync<ResponseDto>();
+            if (response != null && response.IsSuccess)
+            {
+                products = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+
+            }
+            return View(products);
+        }
+
+        public async Task<IActionResult> ProductCreate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductCreate(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productServices.CreateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> ProductEdit(int productId)
+        {
+            ProductDto product = new ProductDto();
+            var response = await _productServices.GetAProductByIdAsync<ResponseDto>(productId);
+            if (response != null && response.IsSuccess)
+            {
+                product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(product);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductEdit(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productServices.UpdateProductAsync<ResponseDto>(model);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+
+                }
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> ProductDelete(int productId)
+        {
+            ProductDto product = new ProductDto();
+            var response = await _productServices.GetAProductByIdAsync<ResponseDto>(productId);
+            if (response != null && response.IsSuccess)
+            {
+                product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+                return View(product);
+            }
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ProductDelete(ProductDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _productServices.DeleteProductAsync<ResponseDto>(model.ProductId);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(ProductIndex));
+
+                }
+            }
+            return View(model);
+        }
+    }
+}
